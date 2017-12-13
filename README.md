ac391e3e843b5e3f3a9cf8b823659d9fbd11c5c9
Nick Fitzgerald <fitzgen@gmail.com>
2017-12-13 14:53:45 -0600

    Fill out doc comments and things

--- README.md
+++ README.md
@@ -1,5 +1,61 @@
 # wasm-nm
 
-TODO FITZGEN
+[![](https://docs.rs/wasm-nm/badge.svg)](https://docs.rs/wasm-nm/) [![](https://img.shields.io/crates/v/wasm-nm.svg)](https://crates.io/crates/wasm-nm) [![](https://img.shields.io/crates/d/wasm-nm.png)](https://crates.io/crates/wasm-nm) [![Build Status](https://travis-ci.org/fitzgen/wasm-nm.png?branch=master)](https://travis-ci.org/fitzgen/wasm-nm)
+
+List the imported and exported symbols within a wasm file.
+
+* [Library](#library)
+* [Executable](#executable)
+* [License](#license)
+* [Contributing](#contributing)
+
+### Executable
+
+To install the `wasm-nm` executable, run
+
+```text
+$ cargo install wasm-nm
+```
+
+For information on using the `wasm-nm` executable, run
+
+```text
+$ wasm-nm --help
+```
+
+### Library
+
+To use `wasm-nm` as a library, add this to your `Cargo.toml`:
+
+```toml
+[dependencies.wasm-nm]
+# Do not build the executable.
+default-features = false
+```
+
+See [docs.rs/wasm-nm][docs] for API documentation.
+
+[docs]: https://docs.rs/wasm-nm
+
+### License
+
+Licensed under either of
+
+ * [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+ * [MIT license](http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contributing
+
+See
+[CONTRIBUTING.md](https://github.com/fitzgen/wasm-nm/blob/master/CONTRIBUTING.md)
+for hacking.
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
+
 
 License: Apache-2.0/MIT
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,63 @@
-//! TODO FITZGEN
+/*!
+
+[![](https://docs.rs/wasm-nm/badge.svg)](https://docs.rs/wasm-nm/) [![](https://img.shields.io/crates/v/wasm-nm.svg)](https://crates.io/crates/wasm-nm) [![](https://img.shields.io/crates/d/wasm-nm.png)](https://crates.io/crates/wasm-nm) [![Build Status](https://travis-ci.org/fitzgen/wasm-nm.png?branch=master)](https://travis-ci.org/fitzgen/wasm-nm)
+
+List the imported and exported symbols within a wasm file.
+
+* [Library](#library)
+* [Executable](#executable)
+* [License](#license)
+* [Contributing](#contributing)
+
+## Executable
+
+To install the `wasm-nm` executable, run
+
+```text
+$ cargo install wasm-nm
+```
+
+For information on using the `wasm-nm` executable, run
+
+```text
+$ wasm-nm --help
+```
+
+## Library
+
+To use `wasm-nm` as a library, add this to your `Cargo.toml`:
+
+```toml
+[dependencies.wasm-nm]
+# Do not build the executable.
+default-features = false
+```
+
+See [docs.rs/wasm-nm][docs] for API documentation.
+
+[docs]: https://docs.rs/wasm-nm
+
+## License
+
+Licensed under either of
+
+ * [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+ * [MIT license](http://opensource.org/licenses/MIT)
+
+at your option.
+
+## Contributing
+
+See
+[CONTRIBUTING.md](https://github.com/fitzgen/wasm-nm/blob/master/CONTRIBUTING.md)
+for hacking.
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
+
+ */
 
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
@@ -12,29 +71,17 @@ use std::fmt;
 use std::io;
 use std::slice;
 
-/*
-Each symbol name is preceded by its value (blanks if undefined).  Unless the -m
-option is specified, this value is followed by one of the following characters,
-representing the symbol type: U (unde- fined), A (absolute), T (text section
-symbol), D (data section symbol), B (bss section symbol), C (common symbol), -
-(for debugger symbol table entries; see -a below), S (symbol in a section other
-than those above), or I (indirect symbol).  If the symbol is local
-(non-external), the symbol's type is instead represented by the corresponding
-lowercase letter.  A lower case u in a dynamic shared library indicates a
-undefined reference to a private external in another module in the same library.
-*/
-
-/// TODO FITZGEN
+// Needed until https://github.com/paritytech/parity-wasm/issues/125 is fixed.
 #[derive(Debug, Fail)]
 #[fail(display = "{:?}", _0)]
-pub struct WasmError(parity_wasm::elements::Error);
+struct WasmError(parity_wasm::elements::Error);
 
-/// TODO FITZGEN
+/// Options for controlling which symbols are iterated over.
 #[derive(Clone, Debug)]
 pub struct Options {
-    /// TODO FITZGEN
+    /// Should imported symbols be iterated over?
     pub imports: bool,
-    /// TODO FITZGEN
+    /// Should exported symbols be iterated over?
     pub exports: bool,
 }
 
@@ -47,7 +94,7 @@ impl Default for Options {
     }
 }
 
-/// TODO FITZGEN
+/// Get the symbols in the given wasm file.
 pub fn symbols<R>(opts: Options, reader: &mut R) -> Result<Symbols, failure::Error>
 where
     R: io::Read,
@@ -56,7 +103,7 @@ where
     Ok(Symbols { opts, module })
 }
 
-/// TODO FITZGEN
+/// The set of symbols in a wasm file.
 pub struct Symbols {
     opts: Options,
     module: Module,
@@ -72,7 +119,7 @@ impl fmt::Debug for Symbols {
 }
 
 impl Symbols {
-    /// TODO FITZGEN
+    /// Iterate over the symbols.
     pub fn iter(&self) -> SymbolsIter {
         SymbolsIter {
             symbols: self,
@@ -81,7 +128,8 @@ impl Symbols {
     }
 }
 
-/// TODO FITZGEN
+/// An iterator returned by `Symbols::iter`, which iterates over the symbols in
+/// a wasm file.
 #[derive(Debug)]
 pub struct SymbolsIter<'a> {
     symbols: &'a Symbols,
@@ -141,13 +189,13 @@ impl<'a> Iterator for SymbolsIter<'a> {
     }
 }
 
-/// TODO FITZGEN
+/// A symbol from a wasm file.
 #[derive(Clone, Debug)]
 pub enum Symbol<'a> {
-    /// TODO FITZGEN
+    /// An imported symbol.
     Import(&'a str),
 
-    /// TODO FITZGEN
+    /// An exported symbol.
     Export(&'a str),
 }
 
@@ -156,13 +204,5 @@ impl<'a> fmt::Display for Symbol<'a> {
         match *self {
             Symbol::Import(s) | Symbol::Export(s) => f.write_str(s),
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
     }
 }
